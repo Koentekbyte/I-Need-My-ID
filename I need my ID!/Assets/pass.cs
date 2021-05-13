788745e5997e4d569d4575446a229e3db14b1038
Koentekbyte <78475300+Koentekbyte@users.noreply.github.com>
2021-05-13 20:43:31 +0100

    second commit
    did some work on catchat

--- I need my ID!/Assets/pass.cs
+++ I need my ID!/Assets/pass.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class pass : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class pass : MonoBehaviour {
+ 
+    public InputField inputfield;  
+ 
+    public void CheckInput() {
+        if (inputfield.text == "password123")      // check inputfield contains the string password
+        {
+            Debug.Log("Password accepted");     // just a debug.Log to show that the password is correct (can be removed)
+            SceneManager.LoadScene("Home");  // fill in the name of the scene you want to load
+        }
     }
 }
+ 
+ 
+
