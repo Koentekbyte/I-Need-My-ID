c0e3a70a0ea99176833527fcb4cbb29b7eac48cf
Koentekbyte <78475300+Koentekbyte@users.noreply.github.com>
2021-05-14 21:58:24 +0100

    Monee
    I added the banking app monee

--- I need my ID!/Assets/block_leak.cs
+++ I need my ID!/Assets/block_leak.cs
@@ -1,18 +1,13 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class block_leak : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public GameObject block;
+    public GameObject leak;
+    
+    public void pass(){
+        block.active = true;
     }
 }
---
+++ I need my ID!/Assets/redirect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class redirect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void re_catchat()
+    {
+        SceneManager.LoadScene("Catchat");
+    }
+
+    
+    
+}
